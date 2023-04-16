e64a604e7303bec0b792e371f35ae78287ef6150
dherzey <jdtganding0998@outlook.com>
2023-04-16 15:25:00 +0000

    Fixed Oscars model date

--- dbt/models/intermediate/bechdel_oscars_model.sql
+++ dbt/models/intermediate/bechdel_oscars_model.sql
@@ -6,11 +6,23 @@ WITH bechdel_imdb AS (
 ),
 
 oscars AS (
+    SELECT 
+        Movie,
+        AwardCategory,
+        AWardStatus,        
+        CAST(AwardYear AS INT64) AS AwardYear,
+        AwardCeremonyNum
+    FROM {{ source('staging', 'oscars') }}
+    WHERE AwardYear NOT LIKE '%/%'
+),
+
+oscars_doubledate AS (
     SELECT *
     FROM {{ source('staging', 'oscars') }}
+    WHERE AwardYear LIKE '%/%'
 )
 
-SELECT
+SELECT DISTINCT
     b.imdbid,
     b.primaryTitle,
     b.startYear,
@@ -21,4 +33,18 @@ SELECT
     b.bechdelRatingRemark,
 FROM bechdel_imdb AS b
     LEFT JOIN oscars AS o
+    ON b.primaryTitle = o.Movie
+    AND b.startYear = o.AwardYear
+UNION ALL
+SELECT DISTINCT
+    b.imdbid,
+    b.primaryTitle,
+    b.startYear,
+    o.AwardCeremonyNum AS oscarsCeremony,
+    o.AwardCategory AS oscarsCategory,
+    o.AwardStatus AS oscarsStatus,
+    b.bechdelRating,
+    b.bechdelRatingRemark,
+FROM bechdel_imdb AS b
+    LEFT JOIN oscars_doubledate AS o
     ON b.primaryTitle = o.Movie
