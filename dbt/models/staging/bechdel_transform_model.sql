04033fbd4b6d0cf53e9057491bd67755f469d591
JERSEY DIANNE GANDING <jdtganding0998@outlook.com>
2023-04-21 01:09:32 +0800

    Modified bechdel model

--- dbt/models/staging/bechdel_transform_model.sql
+++ dbt/models/staging/bechdel_transform_model.sql
@@ -1,9 +1,63 @@
 {{ config(materialized='view') }}
 
+WITH bechdel_new AS (
+    SELECT 
+        title, 
+        CAST(imdbid AS INT64) AS imdbid,
+        id,
+        year,
+        rating,
+        CASE
+            WHEN rating = 3 THEN 'passed'
+            ELSE "failed"
+        END AS ratingRemark
+    FROM {{ source('staging', 'bechdel') }}
+),
+
+non_unique AS (
+    -- there are 18 non-unique values for the imdbid 
+    -- and upon checking, there seemed to be duplicates
+    -- or errors in associating imdbid correctly
+    SELECT imdbid, COUNT(*)
+    FROM bechdel_new
+    GROUP BY imdbid
+    HAVING COUNT(*) > 1
+),
+
+unique_full AS (
+    SELECT *
+    FROM bechdel_new
+    WHERE imdbid NOT IN (
+        SELECT imdbid FROM non_unique)
+    AND imdbid IS NOT NULL
+),
+
+rank_title AS (
+    -- get the first record from a duplicate
+    -- in case of different ratings, get the
+    -- highest rating by default
+    SELECT *,
+        RANK() OVER(PARTITION BY imdbid ORDER BY rating DESC) 
+        AS rankFinal
+    FROM (
+        SELECT *,
+            RANK() OVER(PARTITION BY imdbid, rating ORDER BY id) 
+            AS rankTitle
+        FROM bechdel_new
+        WHERE imdbid IN (SELECT imdbid FROM non_unique)
+    )
+    WHERE rankTitle = 1
+)
+
 SELECT *,
-    CASE
-        WHEN rating = 3 THEN 'passed'
-        ELSE "failed"
-    END AS ratingRemark
-FROM {{ source('staging', 'bechdel') }}
-WHERE imdbid IS NOT NULL
+FROM unique_full
+UNION ALL
+SELECT
+    title, 
+    imdbid,
+    id,
+    year,
+    rating,
+    ratingRemark
+FROM rank_title
+WHERE rankFinal = 1
